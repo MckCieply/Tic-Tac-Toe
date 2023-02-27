683ea42295ff82e8091cbd739c57d5661bcdfeb3
Alex <mwppl123@gmail.com>
2023-02-27 12:09:34 +0100

    Creating home page

--- games/games/settings.py
+++ games/games/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tictactoe.apps.TictactoeConfig',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'games.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'games/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
